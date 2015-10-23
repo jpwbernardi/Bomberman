657c836b0dcf86e13976797e89e7855f7000ae72
João Winckler <Baymax@localhost.localdomain>
2015-10-23 12:40:14 -0200

    Movimento (WIP)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <stdio.h>
 using namespace std;
 
+#define tUP 0
+#define tDOWN 1
+#define tLEFT 2
+#define tRIGHT 3
+#define SPEED 10
+
 const int LARGURA_TELA = 640;
 const int ALTURA_TELA = 480;
 
@@ -11,7 +17,23 @@ ALLEGRO_BITMAP *imagem = NULL;
 ALLEGRO_BITMAP *fundo = NULL;
 ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
 
+bool move[4];
+
 int inicializar();
+
+void teclas(int keycode, bool estado){
+  if(keycode == ALLEGRO_KEY_UP) move[tUP] = estado;
+  if(keycode == ALLEGRO_KEY_DOWN) move[tDOWN] = estado;
+  if(keycode == ALLEGRO_KEY_LEFT) move[tLEFT] = estado;
+  if(keycode == ALLEGRO_KEY_RIGHT) move[tRIGHT] = estado;
+}
+
+void atualiza(int &x, int &y){
+  if(move[tUP]) y -= SPEED;
+  if(move[tDOWN]) y += SPEED;
+  if(move[tLEFT]) x -= SPEED;
+  if(move[tRIGHT]) x += SPEED;
+}
 
 int main(void){
   bool sair = false;
@@ -25,25 +47,11 @@ int main(void){
     while(!al_is_event_queue_empty(fila_eventos)){
       ALLEGRO_EVENT evento;
       al_wait_for_event(fila_eventos, &evento);
-
-      if (evento.type == ALLEGRO_EVENT_KEY_DOWN){
-        switch(evento.keyboard.keycode){
-          case ALLEGRO_KEY_UP:
-            y -= 50;
-            break;
-          case ALLEGRO_KEY_DOWN:
-            y += 50;
-            break;
-          case ALLEGRO_KEY_LEFT:
-            x -= 50;
-            break;
-          case ALLEGRO_KEY_RIGHT:
-            x += 50;
-            break;
-        }
-      }
-      if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) sair = true;
+      if (evento.type == ALLEGRO_EVENT_KEY_DOWN) teclas(evento.keyboard.keycode, true);
+      else if(evento.type == ALLEGRO_EVENT_KEY_UP) teclas(evento.keyboard.keycode, false);
+      else if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) sair = true;
     }
+    atualiza(x, y);
     al_draw_bitmap(fundo, 0, 0, 0);
     al_draw_bitmap(imagem, x, y, 0);
     al_flip_display();
@@ -64,6 +72,8 @@ int inicializar(){
   imagem = al_load_bitmap("bb-1.png");
   fundo = al_load_bitmap("bb.jpg");
   fila_eventos = al_create_event_queue();
+
+  for(int i = 0; i < 4; i++) move[i] = false;
 
   if (!janela){ fprintf(stderr, "Falha ao criar janela.\n"); return 0; }
   if (!imagem || !fundo){ fprintf(stderr, "Falha ao carregar o arquivo de imagem.\n"); al_destroy_display(janela); return 0; }
